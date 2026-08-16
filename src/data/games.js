f65d5969e7ad83f6e4a4c05e89039bdcd5b513ad
Zikri Akmal S <zikriakmale@gmail.com>
2026-08-16 19:03:58 +0800

    feat: add games data and placeholder thumbnail

---
+++ src/data/games.js
@@ -0,0 +1,11 @@
+const games = [
+  {
+    id: 1,
+    title: "Dodge the Creeps",
+    description: "Avoid the creeps as long as you can! A classic arcade survival game made with Godot.",
+    thumbnail: "/thumbnails/dodge-the-creeps.png",
+    url: "https://your-hosted-game-url.com",
+  },
+]
+
+export default games
