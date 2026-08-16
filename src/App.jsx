331652363f2117e87b0a727d7fc5fc2945bcd96f
Zikri Akmal S <zikriakmale@gmail.com>
2026-08-16 19:07:19 +0800

    feat: wire up app with game grid and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import './App.css'
+import GameCard from './components/GameCard'
+import games from './data/games'
+
 function App() {
-  return <div>Portal</div>
+  return (
+    <div>
+      <header className="portal-header">
+        <h1>Game Portal</h1>
+        <p>Pick a game and start playing</p>
+      </header>
+      <main className="game-grid">
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </main>
+    </div>
+  )
 }
 
 export default App
