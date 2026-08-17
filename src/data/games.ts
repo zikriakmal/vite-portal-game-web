d5562875160bb51779e55221dab17da790e4977c
Zikri Akmal S <zikriakmale@gmail.com>
2026-08-17 11:36:10 +0800

    feat: change thumbnail to dodge the creepas

--- src/data/games.ts
+++ src/data/games.ts
@@ -12,7 +12,7 @@ const games: Game[] = [
     id: 1,
     title: "Dodge the Creeps",
     description: "Avoid the creeps as long as you can! A classic arcade survival game made with Godot.",
-    thumbnail: "/thumbnails/dodge-the-creeps.png",
+    thumbnail: "/thumbnails/dodge-the-creepas.png",
     url: "/games/dodge-the-creeps/godot-game-web.html",
     engine: "Godot",
   },
